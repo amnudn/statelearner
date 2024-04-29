5715e3c6d4022b7d0b0e7fb76e884decba154054
Anders Munch <amnudn@gmail.com>
2024-04-29 10:31:24 +0200

    response to TAG's comments

--- figures/figure-multi-state-process.R
+++ figures/figure-multi-state-process.R
@@ -3,9 +3,9 @@
 ## Author: Thomas Alexander Gerds
 ## Created: Jul 27 2023 (10:04) 
 ## Version: 
-## Last-Updated: Oct  9 2023 (18:22) 
+## Last-Updated: Apr 29 2024 (10:30) 
 ##           By: Anders Munch
-##     Update #: 2
+##     Update #: 8
 #----------------------------------------------------------------------
 ## 
 ### Commentary: 
@@ -15,11 +15,15 @@
 ## 
 ### Code:
 library(prodlim)
+library(here)
 nTrans <- 3
 stateLabels = c("Initial","Cause 1", "Cause 2", "Censored")
 crHist <- Hist(time = 1:nTrans, event = list(from = rep("1", nTrans), to = stateLabels[-1]))
-pdf("~/research/SuperVision/Anders/survival-loss/statelearner/figure-multi-state-process.pdf")
-plot(crHist,stateLabels = c("Initial","Cause 1", "Cause 2", "Censored"),arrowLabels = FALSE)
+try(setwd("~/research/SuperVision/Anders/survival-loss/statelearner/figures"))
+try(setwd(here("figures")))
+pdf("figure-multi-state-process.pdf")
+plot(crHist,stateLabels = stateLabels,arrowLabels = FALSE,
+     tagBoxes = FALSE) ## Make true and/or change to -1?
 dev.off()
 
 ######################################################################
